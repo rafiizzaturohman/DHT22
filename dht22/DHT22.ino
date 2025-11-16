5be9e18c52798c23a8e6eb8aae6040db456c2617
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-11-16 18:55:32 +0700

    DHT22

--- dht22/DHT22.ino
+++ dht22/DHT22.ino
@@ -4,8 +4,6 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#include <string>
-
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
